74d82ad1d9dff5899bc5cd77ee4d4929d72fbf60
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2022-09-11 12:56:57 +0600

    Update Math_class.java

--- OOP/Math_class.java
+++ OOP/Math_class.java
@@ -5,7 +5,10 @@ public class add {
         System.out.println(Math.sqrt(16));
         System.out.println(Math.pow(2,3));
         System.out.println(Math.PI);
-        System.out.println(Math.min(5,2));
-        System.out.println(Math.max(5,2));
+        System.out.println(Math.floor(5.7));
+        System.out.println(Math.ceil(5.2));
+        System.out.println(Math.round(6.7));
+        System.out.println(Math.exp(1));
+        System.out.println(Math.random());
     }
 }
