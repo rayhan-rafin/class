3917e3f92cb559edf843090b333c1cd424ba3ffb
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2022-09-11 13:34:20 +0600

    Create Typecasting.java

---
+++ OOP/Typecasting.java
@@ -0,0 +1,10 @@
+public class add {
+    public static void main (String[] args)
+    {
+        double d = 100.4;
+        int i = (int)d; //explicit type casting required, big to small data type 
+
+        System.out.println("Double value "+d);
+        System.out.println("Integer value "+i);
+    }
+}
