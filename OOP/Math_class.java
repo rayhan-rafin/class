3391c1eb40bf765391cb64522da24806093ab20b
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2022-09-11 12:46:29 +0600

    Create Math_class.java

---
+++ OOP/Math_class.java
@@ -0,0 +1,11 @@
+public class add {
+    public static void main (String[] args)
+    {
+        System.out.println(Math.abs(-40.5));
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.PI);
+        System.out.println(Math.min(5,2));
+        System.out.println(Math.max(5,2));
+    }
+}
