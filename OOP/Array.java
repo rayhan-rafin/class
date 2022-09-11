835b658b4a06849e50477dabfd5528cea75fd921
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2022-09-11 13:09:29 +0600

    Create Array.java

---
+++ OOP/Array.java
@@ -0,0 +1,11 @@
+public class add {
+    public static void main (String[] args)
+    {
+        int [] arr = {10,20,30,40,50};
+
+        for (int i = 0; i<arr.length; i++)
+        {
+            System.out.println("Value are "+arr[i]);
+        }
+    }
+}
