b72b05592fabe44d6206b5b25358b50c6e560022
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2022-09-18 10:13:00 +0600

    Update Typecasting.java

--- OOP/Typecasting.java
+++ OOP/Typecasting.java
@@ -2,7 +2,7 @@ public class add {
     public static void main (String[] args)
     {
         double d = 100.4;
-        int i = (int)d;                          //explicit type casting required, big to small data type 
+        int i = (int)d;                          //explicit type casting required, big to small data type (else auto conversion)
                                                 //precedence:
                                                //double > float > long > int > short > byte
         System.out.println("Double value "+d);
