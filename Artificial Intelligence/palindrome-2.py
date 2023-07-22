547495d95cc02a7b4f53100cdbbf603c12ce5a60
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2023-07-22 23:35:13 +0600

    Create palindrome-2.py

---
+++ Artificial Intelligence/palindrome-2.py
@@ -0,0 +1,14 @@
+count = 0
+z= 100
+a=[]  
+for i in range  (100,1000,1):
+    for j in range (z,1000,1):
+        x=i*j
+        m = str (x)
+        if m not in a:
+            a.append(m)
+            n = m[::-1]
+            if (m==n):
+             count = count +1 
+    z=z+1
+print (count)
