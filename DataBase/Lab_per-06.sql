7b1c8c9f1ce220cd3bec62b93fc2ab6cc6ff069a
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2023-09-21 13:17:49 +0600

    Update Lab_per-06.sql

--- DataBase/Lab_per-06.sql
+++ DataBase/Lab_per-06.sql
@@ -1,3 +1,3 @@
-SELECT customers.name FROM customers
-INNER JOIN orders ON customers.id = orders.customer_id
-HAVING COUNT(*)>2;
+SELECT customers.name FROM customers INNER JOIN orders ON customers.id = orders.customer_id HAVING COUNT(*)>2;
+SELECT orders.customer_id, COUNT(orders.order_id) AS total_orders FROM orders GROUP BY orders.customer_id order by total_orders desc;
+(2 WITH NAME) SELECT customers.name, COUNT(*) FROM customers INNER JOIN orders ON customers.id = orders.customer_id GROUP BY orders.customer_id ORDER BY COUNT(*) DESC;
