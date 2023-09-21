6c0ea49467c8b88cb8c5d8eb5547dd65806d27ef
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2023-09-21 12:55:17 +0600

    Update Lab-06.sql

--- DataBase/Lab-06.sql
+++ DataBase/Lab-06.sql
@@ -9,4 +9,13 @@ SELECT customers.id,customers.NAME, orders.order_id,orders.order_date
 from customers
 LEFT JOIN ORDERS ON customers.id = orders.customer_id;
 
-//
+//FULL JOIN USING UNION
+SELECT customers.id,customers.NAME, orders.order_id,orders.order_date
+from customers
+LEFT JOIN ORDERS ON customers.id = orders.customer_id
+
+UNION
+
+SELECT customers.id,customers.NAME, orders.order_id,orders.order_date
+from customers
+RIGHT JOIN ORDERS ON customers.id = orders.customer_id;
