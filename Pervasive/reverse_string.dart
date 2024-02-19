db0f6546b3e801d4fe0055f1e5f85c2809162714
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2024-02-19 15:49:50 +0600

    Update reverse_string.dart

--- Pervasive/reverse_string.dart
+++ Pervasive/reverse_string.dart
@@ -1,11 +1,12 @@
 import 'package:dart4/dart4.dart' as dart4;
 
-void reverseString(String name)
+String reverseString(String name)
 {
   String rev = "";
   for (int i =(name.length)-1 ;i>=0;i--){
     rev = rev+name[i];
   }
+  return rev;
 }
 
 void main()
