95ddad5fd54c3d28cd64530b552fadf0b46cad34
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2024-02-19 15:08:30 +0600

    Create inheritance.dart

---
+++ Pervasive/inheritance.dart
@@ -0,0 +1,44 @@
+import 'package:dart4/dart4.dart' as dart4;
+
+class person
+{
+  String? name;
+  int? age;
+
+}
+
+class doctor extends person
+{
+  List<String>? listofdegree;
+  String? hospitalname;
+
+  void display()
+  {
+    print("Name is : $name");
+    print("Age is : $age");
+    print("Degree is : $listofdegree");
+    print("Hospital name : $hospitalname");
+  }
+}
+
+class specialist extends doctor
+{
+  String? specialization;
+  void display()
+  {
+    super.display();
+    print("Specialization: $specialization");
+  }
+}
+
+
+void main ()
+{
+  specialist sp = specialist();
+  sp.name = "Rayhan";
+  sp.age = 23;
+  sp.listofdegree = ["MBBS","BDDS"];
+  sp.hospitalname = "Delta";
+  sp.specialization = "Eye";
+  sp.display();
+}
