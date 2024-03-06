3edbd83d9b2c52db370dbb266b494406ecb58340
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2024-03-06 09:51:33 +0600

    Update if_else.sh

--- Operating System/if_else.sh
+++ Operating System/if_else.sh
@@ -1,8 +1,9 @@
 num1=10
 num2=20
 
-if [ $num1 -lt $num2 ]; then
-echo "num1 less than num2"
+if [ $num1 -lt $num2 ];
+then
+   echo "num1 less than num2"
 else
-echo "num2 less than num1"
+   echo "num2 less than num1"
 fi
