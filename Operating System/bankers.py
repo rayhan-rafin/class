5e7bc11620c0c9c4bf379fbae2bdd868c393ec14
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2024-05-15 09:51:52 +0600

    Create bankers.py

---
+++ Operating System/bankers.py
@@ -0,0 +1,107 @@
+
+P = 5
+
+
+R = 3
+
+
+def calculateNeed(need, maxm, allot):
+	for i in range(P):
+		for j in range(R):
+			need[i][j] = maxm[i][j] - allot[i][j] 
+
+
+def isSafe(processes, avail, maxm, allot):
+	need = []
+	for i in range(P):
+		l = []
+		for j in range(R):
+			l.append(0)
+		need.append(l)
+		
+	
+	calculateNeed(need, maxm, allot)
+
+	
+	finish = [0] * P                        # Mark all processes as infinish 
+	
+	safeSeq = [0] * P 
+
+	
+	work = [0] * R                         
+	for i in range(R):
+		work[i] = avail[i] 
+
+	# While all processes are not finished 
+	# or system is not in safe state. 
+	count = 0
+	while (count < P):
+		
+		# Find a process which is not finish 
+		# and whose needs can be satisfied 
+		# with current work[] resources. 
+		found = False
+		for p in range(P): 
+		
+			# First check if a process is finished, 
+			# if no, go for next condition 
+			if (finish[p] == 0): 
+			
+				# Check if for all resources 
+				# of current P need is less 
+				# than work
+				for j in range(R):
+					if (need[p][j] > work[j]):
+						break
+					
+				# If all needs of p were satisfied. 
+				if (j == R - 1): 
+				
+					# Add the allocated resources of 
+					# current P to the available/work 
+					# resources i.e.free the resources 
+					for k in range(R): 
+						work[k] += allot[p][k] 
+
+					# Add this process to safe sequence. 
+					safeSeq[count] = p
+					count += 1
+
+					# Mark this p as finished 
+					finish[p] = 1
+
+					found = True
+				
+		# If we could not find a next process 
+		# in safe sequence. 
+		if (found == False):
+			print("System is not in safe state")
+			return False
+		
+	# If system is in safe state then 
+	# safe sequence will be as below 
+	print("System is in safe state.",
+			"\nSafe sequence is: ", end = " ")
+	print(*safeSeq) 
+
+	return True
+
+if __name__ == "__main__":
+    processes = [0, 1, 2, 3, 4]
+    total = [10, 5, 7]  
+
+    maxm = [[7, 5, 3], [3, 2, 2],
+            [9, 0, 2], [2, 2, 2],
+            [4, 3, 3]]
+            
+    allot = [[0, 1, 0], [2, 0, 0],
+             [3, 0, 2], [2, 1, 1],
+             [0, 0, 2]]
+
+    avail = [total[i] - sum(allot[j][i] for j in range(len(processes))) for i in range(len(total))]
+
+    print("The available resources are:", avail)
+    
+    isSafe(processes, avail, maxm, allot) 
+
+
